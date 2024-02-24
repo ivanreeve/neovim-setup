d61e1858d5fb08da6460f7e82ffb8bb1fb91b34c
Ivan Reeve <74288935+ivanreeve@users.noreply.github.com>
2024-02-24 13:30:10 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,20 +5,19 @@
 -- https://vim.rtorr.com/
 
 -- Plugins
-
 local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin','~/AppData/Local/nvim-data/plugins')
 
 -- Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
-Plug('ms-jpq/chadtree', {['branch'] = 'chad', ['do'] = 'python3 -m chadtree deps'})
+Plug('preservim/nerdtree')
+Plug('nvim-treesitter/nvim-treesitter')
 Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) 
 Plug('ryanoasis/vim-devicons')
 Plug('nvim-tree/nvim-web-devicons')
 Plug('nvim-lualine/lualine.nvim')
 Plug('windwp/nvim-autopairs')
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 Plug('lukas-reineke/indent-blankline.nvim')
 
 vim.call('plug#end')
@@ -30,13 +29,10 @@ require('nvim-autopairs').setup()
 require('nvim-treesitter.configs').setup({ highlight = { enable = true, } }) -- Treesitter requires MinGW gcc compiler, :TSInstall <lang> to parse highlighter
 
 -- Keymaps
-vim.keymap.set('n', '<C-Bslash>', ':CHADopen<CR>')
-vim.keymap.set('n', '<F9>', ':let &guifont = substitute(&guifont, \':h\\\\(\\\\d\\\\+\\\\)\', \'\\=":h" . (submatch(1) - 1)\', \'\')<CR>')
-vim.keymap.set('n', '<F10>', ':let &guifont = substitute(&guifont, \':h\\\\(\\\\d\\\\+\\\\)\', \'\\=":h" . (submatch(1) + 1)\', \'\')<CR>')
 
 -- Options
 vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 
-vim.cmd('colorscheme catppuccin-mocha')
+vim.cmd('colorscheme catppuccin-macchiato')
