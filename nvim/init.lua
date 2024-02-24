87f9ed19aa25402628127c72468c8a941d0f7473
Ivan Reeve <74288935+ivanreeve@users.noreply.github.com>
2024-02-24 18:53:30 +0800

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,9 @@
 -- Plugins
 local vim = vim
 local Plug = vim.fn['plug#']
+local PLUGIN_PATH = '~/AppData/Local/nvim-data/plugins'
 
-vim.call('plug#begin','~/AppData/Local/nvim-data/plugins')
+vim.call('plug#begin',PLUGIN_PATH)
 
 -- Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
 Plug('nvim-tree/nvim-tree.lua')
@@ -16,26 +17,31 @@ Plug('nvim-treesitter/nvim-treesitter')
 Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) 
 Plug('ryanoasis/vim-devicons')
 Plug('nvim-tree/nvim-web-devicons')
-Plug('nvim-lualine/lualine.nvim')
 Plug('windwp/nvim-autopairs')
 Plug('lukas-reineke/indent-blankline.nvim')
+Plug('crispgm/nvim-tabline')
 
 vim.call('plug#end')
 
 -- Start Plugins
+require('tabline').setup({
+	show_icon = true,
+	brackets = { ' ', ' ' }
+})
 require('nvim-tree').setup()
-require('ibl').setup()
-require('lualine').setup({ options = { theme = 'palenight' } })
+require('ibl').setup() -- Scope requires treesitter to be set up
 require('nvim-autopairs').setup()
 require('nvim-treesitter.configs').setup({ highlight = { enable = true, } }) -- Treesitter requires MinGW gcc compiler, :TSInstall <lang> to parse highlighter
 
 -- Keymaps
 vim.keymap.set('n', '<F6>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<F7>', ':TagbarToggle<CR>')
 
 -- Options
+vim.cmd('colorscheme catppuccin-macchiato')
+vim.cmd('cd C:/Users/Ivan/Desktop/Ivan/Projects')
+
 vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.autochdir = true
-
-vim.cmd('colorscheme catppuccin-macchiato')
