ece1145a75c3cb9c67eb8298f23c708d4f530b04
Ivan Reeve <74288935+ivanreeve@users.noreply.github.com>
2024-02-25 21:10:20 +0800

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,33 +4,113 @@
 -- https://neovim.io/doc/user/lua-guide.html
 -- https://vim.rtorr.com/
 
--- Plugins | Warning! You may want to install these one by one because some installations may freeze
 
--- A little side note: You may want to visit the README files of each repo for more information/dependencies about each plugin
+
+
+
+-- Plugins | Warning! You may want to install these one by one because some installations may freeze
+-- A little side note: You may want to visit the README files of each repo for more information about each plugin
 
 local vim = vim
 local Plug = vim.fn['plug#']
 local PLUGIN_PATH = '~/AppData/Local/nvim-data/plugins'
 
+
+
 vim.call('plug#begin',PLUGIN_PATH)
 
 -- Shorthand notation; fetches https://github.com/nvim-tree/nvim-tree.lua
-Plug('nvim-tree/nvim-tree.lua')
-Plug('nvim-treesitter/nvim-treesitter')
+
+-- Themes
 Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) 
-Plug('ryanoasis/vim-devicons')
-Plug('nvim-tree/nvim-web-devicons')
-Plug('windwp/nvim-autopairs')
-Plug('lukas-reineke/indent-blankline.nvim')
+
+-- Beautify
 Plug('crispgm/nvim-tabline')
 Plug('nvim-lualine/lualine.nvim')
-Plug('nvim-telescope/telescope.nvim'); Plug('nvim-lua/plenary.nvim'); -- requires BurntSushi/ripgrep, sharkdp/fd to work
+
+-- Glyphs
+Plug('ryanoasis/vim-devicons')
+Plug('nvim-tree/nvim-web-devicons')
+
+-- Files
+Plug('nvim-tree/nvim-tree.lua')
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-lua/plenary.nvim'); -- requires BurntSushi/ripgrep, sharkdp/fd to work
+
+-- Syntax
+Plug('nvim-treesitter/nvim-treesitter')
+Plug('windwp/nvim-autopairs')
+Plug('lukas-reineke/indent-blankline.nvim')
+
+-- Autocompletion
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('L3MON4D3/LuaSnip')
+Plug('saadparwaiz1/cmp_luasnip')
+Plug('simrat39/symbols-outline.nvim')
 
 vim.call('plug#end')
 
--- === Initialize Plugins === --
 
--- LUALINE --
+
+
+
+-- === xxxxxxxx INITIALIZE PLUGINS xxxxxxxx === --
+
+
+-- >> NVIM-CMP << --
+
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+
+
+-- >> LSPCONFIGS << --
+
+-- == Language Server Installations == --
+
+-- 1. Download your preferred language server
+-- 2. Initialize: require('lspconfig').<lsp_name>.setup({})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- ::::PYRIGHT::: --
+
+require('lspconfig').pyright.setup {
+  capabilities = capabilities
+}
+
+-- >> SYMBOLS OUTLINE << --
+
+require("symbols-outline").setup()
+
+
+-- >> LUALINE << --
 
 require('lualine').setup({
   options = {
@@ -46,45 +126,64 @@ require('lualine').setup({
   }
 })
 
--- TABLINE --
+
+-- >> TABLINE << --
 
 require('tabline').setup({
 	show_icon = true,
 	brackets = { ' ', ' ' }
 })
 
--- NVIM TREE --
+
+-- >> NVIM TREE << --
 
 require('nvim-tree').setup()
 
--- INDENT GUIDE --
+
+-- >> INDENT GUIDE < --
  
 require('ibl').setup() -- Scope requires treesitter to be set up
 
--- AUTO PAIRS --
+
+-- >> AUTO PAIRS << --
 
 require('nvim-autopairs').setup()
 
--- TREESITTER --
+
+-- >> TREESITTER << --
 
 require('nvim-treesitter.configs').setup({ highlight = { enable = true, } }) -- Treesitter requires MinGW gcc compiler, :TSInstall <lang> | All, to parse highlighter
+
+
+-- === xxxxxxxx END OF PLUGINS xxxxxxxx === --
+
+
+
+
 
 -- === Keymaps === --
 
 vim.keymap.set('n', '<F1>', ':set number!<CR>')
-vim.keymap.set('n', '<F2>', ':set wrap!<CR>')
-vim.keymap.set('n', '<F3>', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<F1>', ':set number!<CR>')
+vim.keymap.set('n', '<F3>', ':set wrap!<CR>')
+vim.keymap.set('n', '<F5>', ':Telescope find_files<CR>')
 vim.keymap.set('n', '<F6>', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', '<F7>', ':TagbarToggle<CR>')
+vim.keymap.set('n', '<F7>', ':SymbolsOutline<CR>')
 
 
 -- === Commands === --
 
 vim.cmd('colorscheme catppuccin-macchiato')
 vim.cmd('cd C:/Users/Ivan/Desktop/Ivan/Projects')
+vim.cmd('set tabstop=4')    
+vim.cmd('set shiftwidth=4')
+vim.cmd('set expandtab')
 
 -- === Options === --
 
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.autochdir = true
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = false
